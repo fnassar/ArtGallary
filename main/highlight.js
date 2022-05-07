7c4f779a7cb7984848e97295945115a335a1a1b9
fnassar <75119980+fnassar@users.noreply.github.com>
2022-05-07 15:13:15 +0400

    ز

--- main/highlight.js
+++ main/highlight.js
@@ -1,3 +1,4 @@
+/* global AFRAME */
 AFRAME.registerComponent('highlight', {
     init: function() {
         var buttonEls = this.buttonEls = this.el.querySelectorAll('.menu-button');
@@ -19,5 +20,28 @@ AFRAME.registerComponent('highlight', {
         evt.target.setAttribute('material', 'color', '#046de7');
         this.el.addState('clicked');
         evt.target.object3D.scale.set(1.2, 1.2, 1.2);
+    },
+
+    onMouseEnter: function(evt) {
+        var buttonEls = this.buttonEls;
+        evt.target.setAttribute('material', 'color', '#046de7');
+        for (var i = 0; i < buttonEls.length; ++i) {
+            if (evt.target === buttonEls[i]) { continue; }
+            buttonEls[i].setAttribute('material', 'color', 'white');
+        }
+    },
+
+    onMouseLeave: function(evt) {
+        if (this.el.is('clicked')) { return; }
+        evt.target.setAttribute('material', 'color', 'white');
+    },
+
+    reset: function() {
+        var buttonEls = this.buttonEls;
+        for (var i = 0; i < buttonEls.length; ++i) {
+            this.el.removeState('clicked');
+            buttonEls[i].play();
+            buttonEls[i].emit('mouseleave');
+        }
     }
 });
